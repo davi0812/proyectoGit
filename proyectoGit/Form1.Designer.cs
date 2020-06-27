b31f3c4050e1ca9fddb5378b08e643f1b947a54d
carlos gomez <cajo30gomez@gmail.com>
2020-06-26 21:57:05 -0500

    Camaraweb

--- proyectoGit/Form1.Designer.cs
+++ proyectoGit/Form1.Designer.cs
@@ -28,36 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.DarkRed;
-            this.label1.Location = new System.Drawing.Point(349, 194);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 52);
-            this.label1.TabIndex = 0;
-            this.label1.Text = ":c";
+            this.button1.Location = new System.Drawing.Point(322, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Activar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(70, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(589, 270);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- proyectoGit/Form1.cs
+++ proyectoGit/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using AForge.Video;
+using AForge.Video.DirectShow;
 namespace proyectoGit
 {
     public partial class Form1 : Form
@@ -15,6 +16,31 @@ namespace proyectoGit
         public Form1()
         {
             InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice VideoCaptureDevice;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            VideoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[0].MonikerString);
+            VideoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            VideoCaptureDevice.Start();
+
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (VideoCaptureDevice.IsRunning == true)
+                VideoCaptureDevice.Stop();
         }
     }
 }
