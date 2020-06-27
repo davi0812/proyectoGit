8de190fcf14d3dbc8227cff6b4385ba9230a7df5
davi <victorasg56@gmail.com>
2020-06-26 22:01:01 -0500

    Boton con info importante

--- proyectoGit/Form1.cs
+++ proyectoGit/Form1.cs
@@ -16,5 +16,15 @@ namespace proyectoGit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vayanse a la verga");
+        }
     }
 }
